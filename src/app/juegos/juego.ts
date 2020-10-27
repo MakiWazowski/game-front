8ef8e4dcaf5f883fec374c95ad180e6e689a6f2e
MakiWazowski <irenebenitopinel@gmail.com>
2020-10-27 12:06:02 +0100

    creados componentes y clase de juegos y añadida tabla de juegos al html

---
+++ src/app/juegos/juego.ts
@@ -0,0 +1,11 @@
+export class Juego {
+
+    idJuego:number;
+    titulo:string;
+    fechaLanzamiento:string;
+    precio:number;
+    pegi:number;
+    categoria:string;
+    //companies: Company[];
+
+}
