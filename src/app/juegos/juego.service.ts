cd69b04765131839b964c3fa3ffdded6d0cf7301
MakiWazowski <irenebenitopinel@gmail.com>
2020-10-29 12:53:27 +0100

    creados metodo get juego por id y update para editar un juego

--- src/app/juegos/juego.service.ts
+++ src/app/juegos/juego.service.ts
@@ -47,4 +47,31 @@ export class JuegoService {
       })
     );
   }
+
+  //metodo para llamar a un juego por el id
+  getJuego(id:number): Observable<Juego> {
+  
+    return this.http.get<Juego>(`${this.urlServer}juegos/${id}`).pipe(
+      catchError(e => {
+        console.error(`getJuego error: "${e.message}"`);
+        //mensaje de alerta
+        this.alertService.error(`error al consultar el juego por id: "${e.message}"`);
+        return throwError(e);
+      })
+    );
+  }
+
+  //metodo para actualizar un juego (editarlo)
+  updateJuego(juego:Juego): Observable<Juego> {
+  
+    return this.http.put<Juego>(`${this.urlServer}juegos/${juego.idJuego}`, juego, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.error(`updateJuego error: "${e.message}"`);
+        //mensaje de alerta
+        this.alertService.error(`error al actualizar el juego por id: "${e.message}"`);
+        return throwError(e);
+      })
+    );
+  }
+
 }
